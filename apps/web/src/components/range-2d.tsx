6ffc4ecc6b8d750fcde22ade0a1263fb5ca38028
Jose M <menendezpoo@gmail.com>
2024-03-06 14:24:43 -0800

    handle touch on range2d

--- apps/web/src/components/range-2d.tsx
+++ apps/web/src/components/range-2d.tsx
@@ -34,17 +34,30 @@ export function Range2d({
     setMouseDownData({ x, y });
   };
 
+  const handleTouchStart = (e: React.TouchEvent) => {
+    if (mouseDownData) {
+      return;
+    }
+    e.preventDefault();
+    setMouseDownData({ x, y });
+  };
+
   // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
   useEffect(() => {
     let down = true;
-    const mouseMove = (e: MouseEvent) => {
+
+    const touchMove = (e: TouchEvent) =>
+      gestureMove(e.touches[0].clientX, e.touches[0].clientY);
+    const mouseMove = (e: MouseEvent) => gestureMove(e.clientX, e.clientY);
+
+    const gestureMove = (clientX: number, clientY: number) => {
       if (!containerRef.current) {
         return;
       }
       if (mouseDownData && down) {
         const b = containerRef.current.getBoundingClientRect();
-        const x = e.clientX - b.left - 8;
-        const y = e.clientY - b.top - 8;
+        const x = clientX - b.left - 8;
+        const y = clientY - b.top - 8;
         setX(Math.max(0, Math.min(1, x / b.width)));
         setY(Math.max(0, Math.min(1, 1 - y / b.height)));
       }
@@ -58,10 +71,14 @@ export function Range2d({
 
     window.addEventListener("mousemove", mouseMove);
     window.addEventListener("mouseup", mouseUp);
+    window.addEventListener("touchmove", touchMove);
+    window.addEventListener("touchend", mouseUp);
 
     () => {
       window.removeEventListener("mousemove", mouseMove);
       window.removeEventListener("mouseup", mouseUp);
+      window.removeEventListener("touchmove", touchMove);
+      window.removeEventListener("touchend", mouseUp);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [mouseDownData, commit]);
@@ -80,6 +97,7 @@ export function Range2d({
         "before:absolute before:inset-0 before:bg-gradient-to-t before:from-black before:to-transparent",
         className
       )}
+      onTouchStart={handleTouchStart}
       onMouseDown={handleMouseDown}
     >
       <div
