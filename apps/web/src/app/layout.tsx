d1e56f2636c7bf8d0f55d1a98a9e7f907fecdf57
Jose Menendez <jose@definitive.io>
2024-03-07 10:36:37 -0800

    add ga

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "./components/theme-provider";
 import { FaviconateProvider } from "@/components/FaviconateContext";
 import { cn } from "@/lib/utils";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html
+      lang="en"
+      suppressHydrationWarning
+    >
       <body className={cn(inter.className, " overflow-hidden")}>
+        <GoogleAnalytics gaId={"G-DN96F2RSB4"} />
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
